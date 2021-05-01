da93d7380fc7e439f6f8aac73fb7ef57c5e078cf
Manish Rawal <rawalsinghmanish@gmail.com>
2021-05-01 11:39:00 +0545

    'blury-welcome-screen-and-register-button-complete'

--- App.js
+++ App.js
@@ -4,19 +4,8 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppText from "./app/components/AppText";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AppButton from "./app/components/AppButton";
+import AppButton from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AppText>I love react native!@#</AppText>
-      <AppButton title="Login" onPress={() => console.log("Tapped")} />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
+import AppButton from "../components/AppButton";
 
-function WelcomeScreen(props) {
+function WelcomeScreen() {
   return (
     <ImageBackground
+      blurRadius={1.5}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text style={styles.textStyle}>Sell</Text>
+        <Text style={styles.tagline}>Lets Sell it!</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 }
@@ -23,10 +27,9 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
-  loginButton: {
+  buttonsContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
   },
   logo: {
     width: 100,
@@ -39,16 +42,14 @@ const styles = StyleSheet.create({
     top: 5,
     alignItems: "center",
   },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
-  },
-  textStyle: {
+
+  tagline: {
     color: "#ffffff",
-    fontWeight: "bold",
+    fontWeight: "600",
     position: "absolute",
     top: 110,
+    fontSize: 25,
+    paddingVertical: 20,
   },
 });
 
