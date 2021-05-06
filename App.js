6da5108ce41eb0595b3d0f854cc2ca6723fea169
Manish Rawal <rawalsinghmanish@gmail.com>
2021-05-06 17:42:02 +0545

    'TextInput-completed'

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { View } from "react-native";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsSreen from "./app/screens/ListingsSreen";
+
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsSreen />;
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Text } from "react-native";
-import styles from "./styles";
-
+import defaultStyles from "../config/styles";
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
 
 export default AppText;
