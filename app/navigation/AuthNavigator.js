00a86c6a7398a66b886641b4fa06fee639572be6
Manish Rawal <rawalsinghmanish@gmail.com>
2021-08-18 11:28:56 +0545

    implementing-authNavigator-object-using-stack-navigator-for-welcome-and-login-and-register-screens

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -3,16 +3,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import WelcomeScreen from "../screens/WelcomeScreen";
 import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/AccountScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
 const Stack = createStackNavigator();
 
-const AuthNavigator = () => {
+const AuthNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen
+      name="Welcome"
+      component={WelcomeScreen}
+      options={{ headerShown: false }}
+    />
     <Stack.Screen name="Login" component={LoginScreen} />
-    {/* <Stack.Screen name="Register" component={RegisterScreen} /> */}
-  </Stack.Navigator>;
-};
+    <Stack.Screen name="Register" component={RegisterScreen} />
+  </Stack.Navigator>
+);
 
 export default AuthNavigator;
